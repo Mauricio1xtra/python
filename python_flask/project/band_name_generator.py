02bd09ccf56b215360a46a661e70762cff1f0a59
mauricio <bestsound2u@gmail.com>
2021-09-23 10:03:17 -0300

    *crei a pasta python_django e project *criei a pasta python_flasl e project *os arquivos foram realocados conforme pastas

---
+++ python_flask/project/band_name_generator.py
@@ -0,0 +1,19 @@
+#1. Create a greeting for your program.
+
+#2. Ask the user for the city that they grew up in.
+
+#3. Ask the user for the name of a pet.
+
+#4. Combine the name of their city and pet and show them their band name.
+
+#5. Make sure the input cursor shows on a new line, see the example at:
+
+print("***Welcome to the Band Name Generator***")
+
+name_city = input("What's name of the city grew up in?\n ")
+print(name_city)
+
+name_pet = input("What's your pet's name?\n ")
+print(name_pet)
+
+print(f"Your band could be {name_city} {name_pet}")
